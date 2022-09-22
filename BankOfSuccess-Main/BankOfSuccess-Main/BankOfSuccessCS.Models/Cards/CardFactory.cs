5f95a5cd990bbe1e016d512cb42340fcc5317015
LAPTOP-TLI6Q7I5\Admin <ishabhandary28@gmail.com>
2022-09-22 10:22:38 +0530

    CardFactory Changed.

--- BankOfSuccess-Main/BankOfSuccess-Main/BankOfSuccessCS.Models/Cards/CardFactory.cs
+++ BankOfSuccess-Main/BankOfSuccess-Main/BankOfSuccessCS.Models/Cards/CardFactory.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BankOfSuccessCS.Models
 {
-    internal class CardFactory
+    public static class CardFactory
     {
+        public static Card GetCard()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
